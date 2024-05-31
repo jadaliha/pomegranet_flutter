41abca83fe981399d8e6a0f65cba85c82e7f4248
sgsm74 <saeed.razii93@gmail.com>
2024-05-31 04:57:13 +0330

    chore: add mock data for category

--- lib/application/features/restaurant/domain/entities/category.dart
+++ lib/application/features/restaurant/domain/entities/category.dart
@@ -7,4 +7,20 @@ class Category extends Equatable {
 
   @override
   List<Object> get props => [title, image];
+
+  static const mockData = [
+    Category(title: 'Burger', image: 'assets/images/categories/burger.png'),
+    Category(title: 'Noodles', image: 'assets/images/categories/noodles.png'),
+    Category(title: 'Pizza', image: 'assets/images/categories/pizza.png'),
+    Category(title: 'Chicken', image: 'assets/images/categories/chicken.png'),
+    Category(title: 'Sushi', image: 'assets/images/categories/sushi.png'),
+    Category(title: 'Meat', image: 'assets/images/categories/meat.png'),
+    Category(title: 'Salad', image: 'assets/images/categories/salad.png'),
+    Category(title: 'Fish', image: 'assets/images/categories/fish.png'),
+    Category(title: 'Rolls', image: 'assets/images/categories/rolls.png'),
+    Category(title: 'Soup', image: 'assets/images/categories/soup.png'),
+    Category(
+        title: 'Kid-Friendly',
+        image: 'assets/images/categories/kid-friendly.png'),
+  ];
 }
