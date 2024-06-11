613e7db69fe81404b506032b1e8ec31e5a882eb5
sgsm74 <saeed.razii93@gmail.com>
2024-06-11 20:18:32 +0330

    chore: remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:pomegranet/application/core/consts/icon_assets.dart';
 import 'package:pomegranet/application/features/restaurant/presentation/pages/home_page.dart';
-import 'package:pomegranet/application/features/restaurant/presentation/pages/widgets/circular_progress_bar.dart';
-import 'package:pomegranet/application/features/restaurant/presentation/pages/widgets/ingrediants_tab.dart';
-import 'package:pomegranet/application/features/restaurant/presentation/pages/widgets/recipe_tab.dart';
-import 'package:pomegranet/application/features/restaurant/presentation/pages/widgets/review_tab.dart';
 
 void main() {
   runApp(const MyApp());
